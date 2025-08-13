59a1a8d470e4718bd6953a8d4d5bc0ebe032d292
Azat201003 <zanelane87@gmail.com>
2025-08-13 23:22:17 +0300

    try to make it more security(я даун )

--- main.hpp
+++ main.hpp
@@ -1,37 +1,89 @@
 #pragma once
 #include <iostream>
 
+template<typename T>
+class convptr {
+public:
+	T* ptr;
+	convptr(T* ptr) : ptr(ptr) {}
+};
+
 class dynarr {
 private:
 	size_t* sizes;
-	size_t MAX_SIZE;
-	void* currentPos;
+	void* current_pos;
 	size_t size = 0;
+	static const size_t give_size = 5252;
+	size_t capacity;
 	
-	void* getPos(size_t index) {
-		void* p = currentPos;
-                for (int j = size; j > index; j--) {
-                        p -= sizes[j-1];
-                }
-		return p;
+	void* get_pos(size_t index) {
+		unsigned long long p = reinterpret_cast<unsigned long long>(current_pos);
+		for (int j = size; j > index; j--) {
+			p -= sizes[j-1];
+		}
+		return reinterpret_cast<void*>(p);
+	}
+	
+	void move_data(void* to) {
+		size_t c = 0;
+		for (size_t i = 0; i < size; i++) {
+			for (size_t j = 0; j < sizes[i]; j++) {
+				c++;
+				char* ptr = static_cast<char*>(current_pos)+c;
+				*(static_cast<char*>(to)+c) = *ptr;
+			}
+		}
+		realloc(current_pos, capacity);
+		current_pos = to;
+	}
+
+	void move_sizes(size_t* to) {
+		for (int i = 0; i < size; i++) {
+			to[i] = sizes[i];
+		}
+		realloc(sizes, capacity);
+		sizes = to;
+	}
+
+	size_t calc_memory_size() {
+		size_t c = 0;
+		for (size_t i = 0; i < size; i++) {
+			c+=sizes[i];
+		}
+		return c;
+	}
+
+	void add_memory(size_t size=give_size) { 
+		void* ptr1 = malloc(capacity+size);
+		move_data(ptr1);
+		size_t* ptr2 = static_cast<size_t*>(malloc(capacity+size));
+		move_sizes(ptr2);
+		capacity += size;
 	}
 public:
-	dynarr(size_t MAX_SIZE) {
-		this->MAX_SIZE = MAX_SIZE;
-		std::cout << "max size setted\n";
-		size_t* sizes_buffer = new size_t[MAX_SIZE];
-		std::cout << "buffer craeted\n";
-		sizes = sizes_buffer;
+	dynarr(size_t capacity = give_size) : capacity(capacity) {
+		this->capacity = capacity;
+		sizes = new size_t(capacity);
+		current_pos = malloc(capacity);
 	}
+
+	~dynarr() {
+		realloc(get_pos(0), capacity);
+		realloc(sizes, capacity);
+	}
+
 	template <typename T>
 	void push_back(T obj) {
+		while (calc_memory_size()+sizeof(T) >= capacity) {
+			add_memory();
+		}
 		if (size == 0){
 			T* first = new T(obj);
-			currentPos = first;
+			current_pos = first;
 		}
-		T* p = static_cast<T*>(currentPos);
+		T* p = static_cast<T*>(current_pos);
 		(*p) = obj;
-		currentPos += sizeof(T);
+		current_pos += sizeof(T);
 		sizes[size] = sizeof(T);
 		size++; 
 	}
@@ -41,20 +93,20 @@ public:
 	}
 
 	void* at(size_t index) {
-		return getPos(index);
+		return get_pos(index);
 	}
 	void pop(size_t index) {
 		size_t deleted_size = sizes[index];
-		for (void* p=getPos(index); p<=currentPos-deleted_size+10; p++) {
-                       	(*static_cast<char*>(p)) = (*static_cast<char*>(p+deleted_size));
-                }
+		for (char* p=static_cast<char*>(get_pos(index)); p<=current_pos-deleted_size+10; p++) {
+			(*p) = *(p+deleted_size);
+		}
 		
 		size--;
 	
 		for (int j = index; j < size; j++) {
-                        sizes[j] = sizes[j+1];
-                }
-		currentPos -= deleted_size;
+			sizes[j] = sizes[j+1];
+        }
+		current_pos = reinterpret_cast<void*>(reinterpret_cast<unsigned long long>(current_pos) - deleted_size); // ponos
 	}
 	void pop() {
 		pop(size);
--- tests/1.cpp
+++ tests/1.cpp
@@ -12,4 +12,12 @@ int main() {
 	arr.push_back('b');
 	arr.pop(0);
 	std::cout << "arr[0] = " << arr.at<char>(0) << std::endl;
+	char* a = "abeme";;
+	//arr.push_back(reinterpret_cast<long long>(a));
+	arr.push_back(convptr(a));
+	//std::cout << reinterpret_cast<long long>(a) << std::endl;
+	arr.pop(0);
+	arr.pop(0);
+	std::cout << "arr.size = " << arr.getSize() << std::endl;
+	std::cout << "arr[0] = " << std::string((arr.at<convptr<char>>(0)).ptr) << std::endl;
 }
